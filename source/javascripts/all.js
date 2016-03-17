9c74c1d955755d729590e326214ec353f4668957
Jeremie <jeremie.bonal@gmail.com>
2016-03-17 19:31:16 +0100

    Menu structure in JS.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+$(document).ready(function(doc){
+  $(".trigger").click(function(e){
+    e.preventDefault();
+    $("."+this.className.split(" ").join("."))
+    .css('opacity', 0)
+    .one('transitionend', function () {
+      $(this).css('display', 'none');
+      $(this).parent().css('display', 'none'); // Hiding all categories
+    });
+    $(this).one('transitionend', function () {
+        $("."+this.id+" .languages").toggleClass("hidden");
+        $(this).parent().css('display', ''); // Re-displaying the one we're in.
+    });
+  });
+});
