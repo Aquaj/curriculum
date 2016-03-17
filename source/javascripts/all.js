39dd468a3c2edce36077d1247b866d367a3f9277
Jeremie <jeremie.bonal@gmail.com>
2016-03-17 23:56:52 +0100

    Icons + back mechanism.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,8 +8,49 @@ $(document).ready(function(doc){
       $(this).parent().css('display', 'none'); // Hiding all categories
     });
     $(this).one('transitionend', function () {
-        $("."+this.id+"> .items").toggleClass("hidden");
+        $('.fa-chevron-left').removeClass("hidden");
+        $("."+this.id+"> .items")
+        .css("opacity", 0)
+        .toggleClass("hidden") // Hiding all categories
+        .css("opacity", "");
         $(this).parent().css('display', ''); // Re-displaying the one we're in.
     });
+  });
+
+  $(".back-link").click(function(e){
+    e.preventDefault();
+    console.log($($(".items:not(.hidden)")[1]));
+    $(".items:not(.hidden)")
+    .last()
+    .parent()
+    .children(".trigger")
+    .css("display", "")
+    .css("opacity", "");
+    $(".items:not(.hidden)")
+    .last()
+    .parent()
+    .parent()
+    .children(".category")
+    .css("display", "")
+    .css("opacity", "");
+    $(".items:not(.hidden)")
+    .last()
+    .parent()
+    .parent()
+    .children(".category")
+    .children(".trigger")
+    .css("display", "")
+    .css("opacity", "");
+    $(".items:not(.hidden)")
+    .last()
+    .toggleClass("hidden");
+
+ //   $($(".items:not(.hidden)")[1])
+   // .css('opacity', 0)
+ //   .one('transitionend', function () {
+   //   $(this).parent().css('display', 'none'); // Hiding all categories
+  //  });
+
+    $($(".items:not(.hidden)")[1]).parent(".items");
   });
 });
